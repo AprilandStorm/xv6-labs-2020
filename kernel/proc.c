217c49a60613724955615865cdefe911e699f3c0
AprilandStorm <2630185199@qq.com>
2025-05-14 16:04:04 +0800

    完成了第二个作业的第一部分

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,7 +126,8 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
-
+  
+  p->kama_syscall_trace = 0;//创建新进程的时候，kama_syscall_trace设置为默认值0
   return p;
 }
 
@@ -296,6 +297,8 @@ fork(void)
   np->state = RUNNABLE;
 
   release(&np->lock);
+
+  np->kama_syscall_trace = p->kama_syscall_trace; //子进程继承父进程的syscall_trace
 
   return pid;
 }
