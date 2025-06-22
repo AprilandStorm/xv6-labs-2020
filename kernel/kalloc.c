8b98a82b8ac8ec6464ef88476e128f99a6e0c55f
AprilandStorm <2630185199@qq.com>
2025-06-22 19:17:56 +0800

    完成第六个实验

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,20 @@ struct {
   struct run *freelist;
 } kmem;
 
+#define PA2PGREF_ID(p) (((p) - KERNBASE) / PGSIZE)
+#define PGREF_MAX_ENTRIES PA2PGREF_ID(PHYSTOP)
+
+struct spinlock pgreflock;//用于pageref数组的锁，防止竞态条件引起的内存泄露
+int pageref[PGREF_MAX_ENTRIES];//从KERNBASE开始到PHYSTOP之间的每个物理页的引用计数
+
+//通过物理地址获取引用计数
+#define PA2PGREF(p) pageref[PA2PGREF_ID((uint64)(p))]
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&pgreflock, "pgref");//初始化锁
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -50,17 +60,24 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  
+  acquire(&pgreflock);
+  if(--PA2PGREF(pa) <= 0){
+    // 当页面的引用计数小于等于0的时候，释放页面
+    // Fill with junk to catch dangling refs.
+    // pa will be memset multiple times if race-condition occurred
+    memset(pa, 1, PGSIZE);
+  
+    r = (struct run*)pa;
+  
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }
+  release(&pgreflock);
+  }
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
-
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
-}
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
@@ -76,7 +93,49 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
+    //新分配的物理页的引用计数为1
+    //这里无需加锁
+    PA2PGREF(r) = 1;
+  }
+
   return (void*)r;
+}
+
+//decrease reference to the page by one if it's more than one, then
+//allocate a new physical page and copy the page into it
+//do nothing and simply return pa when reference count is already
+//less than or equal to 1
+
+void*
+kcopy_n_deref(void* pa){
+  acquire(&pgreflock);
+
+  if(PA2PGREF(pa) <= 1){
+    release(&pgreflock);
+    return pa;
+  }
+
+  //分配新的内存页，并复制旧页中的数据到新页
+  uint64 newpa = (uint64)kalloc();
+  if(newpa == 0){
+    release(&pgreflock);
+    return 0;
+  }
+  memmove((void*)newpa, (void*)pa, PGSIZE);
+
+  //旧页的引用减1
+  PA2PGREF(pa)--;
+
+  release(&pgreflock);
+  return (void*)newpa;
+
+}
+
+//为pa的引用计数增加1
+void krefpage(void* pa){
+  acquire(&pgreflock);
+  PA2PGREF(pa)++;
+  release(&pgreflock);
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "proc.h"
+#include "spinlock.h"
 
 /*
  * the kernel's page table.
@@ -311,7 +313,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+  //char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -319,15 +321,26 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+    if(*pte & PTE_W){
+      //清除父进程的PTE_W标志位，设置PTE_COW标志位表示是一个懒复制页
+      *pte = (*pte & ~PTE_W) | PTE_COW;
+    }
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
+    //将父进程的物理页直接map到子进程
+    //权限设置与父进程一致
+    /*if((mem = kalloc()) == 0)
       goto err;
     memmove(mem, (char*)pa, PGSIZE);
     if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
       kfree(mem);
       goto err;
+    }*/
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
+      goto err;
     }
-  }
+    //将物理页的引用次数增加1
+    krefpage((void*)pa);
+  }  
   return 0;
 
  err:
@@ -357,6 +370,8 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   uint64 n, va0, pa0;
 
   while(len > 0){
+    if(uvmcheckcowpage(dstva))
+      uvmcowcopy(dstva);
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
@@ -439,4 +454,42 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+//检查一个地址指向的页是否是懒复制页
+int
+uvmcheckcowpage(uint64 va){
+  pte_t* pte;
+  struct proc* p = myproc();
+
+  return va < p->sz //在进程内存范围内
+    && ((pte = walk(p->pagetable, va, 0)) != 0)
+    && (*pte & PTE_V)//页表项存在
+    && (*pte & PTE_COW);//页是一个懒复制页
+}
+
+//实复制一个懒复制页，并重新映射为可写
+int
+uvmcowcopy(uint64 va){
+  pte_t* pte;
+  struct proc* p = myproc();
+
+  if((pte = walk(p->pagetable, va, 0)) == 0){
+    panic("uvmcowcopy : walk");
+  }
+
+  //调用kalloc.c中的kcopy_n_deref方法，复制页
+  //如果懒复制页的引用已经为1，则不需要重新分配和复制内存页，只需要清除PTE_COW标记并标记PTE_W
+  uint64 pa = PTE2PA(*pte);
+  uint64 new = (uint64)kcopy_n_deref((void*)pa);//将一个懒复制的页引用变为一个实复制的页
+  if(new == 0)
+    return -1;
+  
+  //重新映射为可写，并清除PTE_COW标记
+  uint64 flags = (PTE_FLAGS(*pte) | PTE_W) & ~PTE_COW;
+  uvmunmap(p->pagetable, PGROUNDDOWN(va), 1, 0);
+  if(mappages(p->pagetable, va, 1, new, flags) == -1){
+    panic("uvmcowcopy : mappages");
+  }
+  return 0;
 }
